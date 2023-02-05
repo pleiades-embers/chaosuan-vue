c721e9476a2a2be496f78f77c50ccd809e7b701d
guokai.embers <guokai.embers@bytedance.com>
2023-02-05 23:13:55 +0800

    update code

--- src/views/dashboard/workplace/components/popular-content.vue
+++ src/views/dashboard/workplace/components/popular-content.vue
@@ -5,36 +5,37 @@
       :header-style="{ paddingBottom: '0' }"
       :body-style="{ padding: '17px 20px 21px 20px' }"
     >
-      <template #title>
-        {{ $t('workplace.popularContent') }}
-      </template>
-      <template #extra>
-        <a-link>{{ $t('workplace.viewMore') }}</a-link>
-      </template>
       <a-space direction="vertical" :size="10" fill>
-        <a-radio-group
-          v-model:model-value="type"
-          type="button"
-          @change="typeChange as any"
+        <a-trigger
+trigger="click" :unmount-on-close="false"
+          auto-fit-popup-width
         >
-          <a-radio value="text">
-            {{ $t('workplace.popularContent.text') }}
-          </a-radio>
-          <a-radio value="image">
-            {{ $t('workplace.popularContent.image') }}
-          </a-radio>
-          <a-radio value="video">
-            {{ $t('workplace.popularContent.video') }}
-          </a-radio>
-        </a-radio-group>
+          <a-input
+            placeholder='可按关键字模糊搜索,多个关键字用","隔开;展开高级搜索可使用更多搜索条件'
+            allow-clear
+          >
+            <template #prefix>
+              <icon-search />
+            </template>
+             <template #suffix>
+              <div>高级搜索</div>
+              <icon-down />
+            </template>
+          </a-input>
+          <template #content>
+            <div class="demo-basic">
+              <a-empty />
+            </div>
+          </template>
+        </a-trigger>
         <a-table
           :data="renderList"
           :pagination="false"
           :bordered="false"
+          :row-selection="rowSelection"
           :scroll="{ x: '100%', y: '264px' }"
         >
           <template #columns>
-            <a-table-column title="排名" data-index="key"></a-table-column>
             <a-table-column title="内容标题" data-index="title">
               <template #cell="{ record }">
                 <a-typography-paragraph
@@ -65,6 +66,17 @@
                 </div>
               </template>
             </a-table-column>
+            <a-table-column title="操作" data-index="op" fixed="right">
+              <template #cell="{ record }">
+                <div>
+                  {{ record?.op }}
+                  <a-space>
+                    <a-button type="primary" size="small">详情</a-button>
+                    <a-button type="primary" size="small">原件 </a-button>
+                  </a-space>
+                </div>
+              </template>
+            </a-table-column>
           </template>
         </a-table>
       </a-space>
@@ -73,46 +85,54 @@
 </template>
 
 <script lang="ts" setup>
-  import { ref } from 'vue';
-  import useLoading from '@/hooks/loading';
-  import { queryPopularList } from '@/api/dashboard';
-  import type { TableData } from '@arco-design/web-vue/es/table/interface';
+import { ref, reactive } from 'vue';
+import useLoading from '@/hooks/loading';
+import { queryPopularList } from '@/api/dashboard';
+import type { TableData } from '@arco-design/web-vue/es/table/interface';
 
-  const type = ref('text');
-  const { loading, setLoading } = useLoading();
-  const renderList = ref<TableData[]>();
-  const fetchData = async (contentType: string) => {
-    try {
-      setLoading(true);
-      const { data } = await queryPopularList({ type: contentType });
-      renderList.value = data;
-    } catch (err) {
-      // you can report use errorHandler or other
-    } finally {
-      setLoading(false);
-    }
-  };
-  const typeChange = (contentType: string) => {
-    fetchData(contentType);
-  };
-  fetchData('text');
+const { loading, setLoading } = useLoading();
+const rowSelection = reactive({
+  type: 'checkbox',
+  showCheckedAll: true,
+  onlyCurrent: false,
+});
+const renderList = ref<TableData[]>();
+const fetchData = async (contentType: string) => {
+  try {
+    setLoading(true);
+    const { data } = await queryPopularList({ type: contentType });
+    renderList.value = data;
+  } catch (err) {
+    // you can report use errorHandler or other
+  } finally {
+    setLoading(false);
+  }
+};
+
+fetchData('text');
 </script>
 
 <style scoped lang="less">
-  .general-card {
-    min-height: 395px;
+.general-card {
+  min-height: 395px;
+}
+:deep(.arco-table-tr) {
+  height: 44px;
+  .arco-typography {
+    margin-bottom: 0;
   }
-  :deep(.arco-table-tr) {
-    height: 44px;
-    .arco-typography {
-      margin-bottom: 0;
-    }
+}
+.increases-cell {
+  display: flex;
+  align-items: center;
+  span {
+    margin-right: 4px;
   }
-  .increases-cell {
-    display: flex;
-    align-items: center;
-    span {
-      margin-right: 4px;
-    }
-  }
+}
+.demo-basic {
+  padding: 10px;
+  background-color: var(--color-bg-popup);
+  border-radius: 4px;
+  box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.15);
+}
 </style>
